2b6d955529c9341289f4a83d27c84f2b8a32971c
Aniket Gupta <157786329+anixet-14@users.noreply.github.com>
2025-07-26 21:01:57 +0530

    Update Ball.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,26 +1,26 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
-  Decal,
   Float,
-  OrbitControls,
-  Preload,
+  Decal,
   useTexture,
+  Preload,
+  OrbitControls,
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
+import { technologies } from "../constants";
 
-const Ball = (props) => {
-  const [decal] = useTexture([props.imgUrl]);
+// === Ball Component ===
+const Ball = ({ icon, position }) => {
+  const [decal] = useTexture([icon]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+    <Float speed={1.5} rotationIntensity={1.5} floatIntensity={2}>
+      <mesh position={position} scale={1.5} castShadow receiveShadow>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
@@ -37,21 +37,51 @@ const Ball = (props) => {
   );
 };
 
-const BallCanvas = ({ icon }) => {
+// === Ball Cloud Layout ===
+const BallCloud = () => {
+  const spacing = 3.5;
+  const perRow = 4;
+  const positions = useMemo(() => {
+    return technologies.map((_, index) => {
+      const x = (index % perRow) * spacing - (spacing * (perRow - 1)) / 2;
+      const y =
+        -Math.floor(index / perRow) * spacing +
+        (spacing * (Math.ceil(technologies.length / perRow) - 1)) / 2;
+      const z = (index % 2 === 0 ? 0 : -1.5);
+      return [x, y, z];
+    });
+  }, []);
+
+  return technologies.map((tech, i) => (
+    <Ball key={tech.name} icon={tech.icon} position={positions[i]} />
+  ));
+};
+
+// === Tech Canvas ===
+const TechCanvas = () => {
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
+      shadows
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
+      camera={{ position: [0, 0, 15], fov: 50 }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
+        <ambientLight intensity={0.8} />
+        <directionalLight position={[5, 5, 5]} intensity={2} />
+        <OrbitControls
+          enableZoom={false}
+          autoRotate
+          autoRotateSpeed={1}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <BallCloud />
       </Suspense>
-
       <Preload all />
     </Canvas>
   );
 };
 
-export default BallCanvas;
+export default TechCanvas;
