6af5a0a30730402dc0b4ad03f588c37a7bb5aa78
Aniket Gupta <157786329+anixet-14@users.noreply.github.com>
2025-07-26 19:25:39 +0530

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -27,7 +27,7 @@ const Computers = ({ isMobile }) => {
       {/* 3D Model */}
       <primitive
         object={computer.scene}
-        scale={isMobile ? 1 : 1.5}
+        scale={isMobile ? 0.4 : 1.2}
         position={isMobile ? [0, -1.8, 0] : [0, -2.2, 0]}
         rotation={[0, 0.4, 0]}
       />
