f01426eb4c9f1e89948f20d4553327261f838e25
Aniket Gupta <157786329+anixet-14@users.noreply.github.com>
2025-07-26 21:08:28 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
-import TechCanvas from "./canvas/balls"; // Updated import path to new balls.jsx
+import TechCanvas from "./canvas/Ball"; // Updated import path to new balls.jsx
 
 const Tech = () => {
   return (
